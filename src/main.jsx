43bb8b3793216b275e6f509850abdf46b2fbf31c
MariaPinchasi <135972701+MariaPinchasi@users.noreply.github.com>
2023-11-21 08:27:01 +0200

    style

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AppProvider } from './context/context.jsx'
 
+import './style/utilities.css'
+import './style/app.css'
+import './style/mobile.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AppProvider>
